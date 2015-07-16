2e8b17d7c840a69390567372d92d14fddd7c6577
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-07-16 16:44:42 +0200

    move numpy json encoder

---
+++ json_encoder.py
@@ -0,0 +1,29 @@
+__author__ = 'Samuel Gratzl'
+
+import numpy as np
+
+class NumpyTablesEncoder(object):
+  def __contains__(self, obj):
+    if isinstance(obj, np.ndarray):
+      return True
+    if isinstance(obj, np.generic):
+      return True
+    return False
+
+  def __call__(self, obj, base_encoder):
+    if isinstance(obj, np.ndarray):
+      if obj.ndim == 1:
+        return [x for x in obj]
+      else:
+        return [base_encoder.default(obj[i]) for i in range(obj.shape[0])]
+    if isinstance(obj, np.generic):
+      a = np.asscalar(obj)
+      if isinstance(a, float) and np.isnan(a):
+        return None
+      return a
+    return None
+
+n = NumpyTablesEncoder()
+
+def create():
+  return n
