7988af1402e589f7e805c377d9d8c1e9c821434d
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-07-24 11:50:26 +0200

    start with creating uwsgi config files

--- _deploy/setup.sh
+++ _deploy/setup.sh
@@ -1,14 +1,18 @@
 #!/usr/bin/env bash
 
+function sedeasy {
+  sed "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3 $4
+}
+
 function install_apt_dependencies {
   if [ -f debian.txt ] ; then
     echo "--- installing apt dependencies ---"
     wd="`pwd`"
     cd /tmp #switch to tmp directory
     set -vx #to turn echoing on and
-    sudo apt-get install -y python-pip python-dev zlib1g-dev cython `cat $wd/debian.txt`
+    sudo apt-get install -y python-pip python-dev zlib1g-dev cython `cat ${wd}/debian.txt`
     set +vx #to turn them both off
-    cd $wd
+    cd ${wd}
     rm debian.txt
   fi
 }
@@ -17,17 +21,27 @@ function install_pip_dependencies {
     echo "--- installing pip dependencies ---"
     wd="`pwd`"
     cd /tmp #switch to tmp directory
-    sudo pip install -r $wd/requirements.txt
+    sudo pip install -r ${wd}/requirements.txt
     set -vx #to turn echoing on and
-    cd $wd
+    cd ${wd}
     rm requirements.txt
   fi
+}
+
+function create_uwsgi_conf {
+  local name=$1
+  sedeasy caleydo_web $1 caleydo_web_nginx.in.conf caleydo_web_nginx.conf
+  sedeasy caleydo_web $1 caleydo_web_uwsgi.in.ini caleydo_web_uwsgi.ini
 }
 
 function create_virtualenv {
   echo "--- creating virtual environment ---"
   wd="`pwd`"
-  sudo pip install virtualenv
+  if hash virtualenv 2>/dev/null; then
+     echo "virtualenv already installed"
+  else
+    sudo pip install virtualenv
+  fi
   virtualenv venv
   source venv/bin/activate
 }
@@ -48,3 +62,6 @@ create_virtualenv
 install_pip_dependencies
 deactivate_virtualenv
 create_run_script
+
+name=${PWD##*/}
+create_uwsgi_conf ${name}
