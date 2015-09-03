53ed1bd991324f807acca8919c77101896613fcb
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-09-03 16:08:10 +0200

    improve setup script

--- _deploy/setup.sh
+++ _deploy/setup.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env bash
 
+#search for the right parent directory such that we have a common start directory
+while [[ ! -f "run.sh" ]] && [[ ! -f "Vagrantfile" ]]
+do
+  cd ..
+done
+
+basedir=`pwd`
+
 function sedeasy {
   sed -i "s/$(echo $1 | sed -e 's/\([[\/.*]\|\]\)/\\&/g')/$(echo $2 | sed -e 's/[\/&]/\\&/g')/g" $3
 }
@@ -7,47 +15,44 @@ function sedeasy {
 function install_apt_dependencies {
   if [ -f debian.txt ] ; then
     echo "--- installing apt dependencies ---"
-    wd="`pwd`"
     cd /tmp #switch to tmp directory
     set -vx #to turn echoing on and
-    sudo apt-get install -y supervisor python-pip python-dev zlib1g-dev `cat ${wd}/debian.txt`
+    sudo apt-get install -y supervisor python-pip python-dev zlib1g-dev `cat ${basedir}/debian.txt`
     set +vx #to turn them both off
-    cd ${wd}
+    cd ${basedir}
     rm debian.txt
   fi
 }
 function install_pip_dependencies {
   if [ -f requirements.txt ] ; then
     echo "--- installing pip dependencies ---"
-    wd="`pwd`"
     cd /tmp #switch to tmp directory
-    pip install -r ${wd}/requirements.txt
+    pip install -r ${basedir}/requirements.txt
     set -vx #to turn echoing on and
-    cd ${wd}
+    cd ${basedir}
     rm requirements.txt
   fi
 }
 
 function create_server_config {
   local name=$1
-  wd="`pwd`"
   cd /tmp #switch to tmp directory
   pip install gunicorn setproctitle
   set -vx #to turn echoing on and
-  cd ${wd}
+  cd ${basedir}
 
   cp gunicorn_start.in.sh gunicorn_start.sh
-  sedeasy /var/www/caleydo_app ${wd} gunicorn_start.sh
+  sedeasy /var/www/caleydo_app ${basedir} gunicorn_start.sh
   sedeasy caleydo_app ${name} gunicorn_start.sh
   chmod +x gunicorn_start.sh
 
 
   cp supervisor.in.conf supervisor.conf
-  sedeasy /var/www/caleydo_app ${wd} supervisor.conf
+  sedeasy /var/www/caleydo_app ${basedir} supervisor.conf
   sedeasy caleydo_app ${name} supervisor.conf
 
   #create the supervisor config
-  sudo ln -s ${wd}/supervisor.conf /etc/supervisor/conf.d/${name}.conf
+  sudo ln -s ${basedir}/supervisor.conf /etc/supervisor/conf.d/${name}.conf
   #reread the list of elements
   sudo supervisorctl reread
   sudo supervisorctl update
@@ -55,11 +60,27 @@ function create_server_config {
   #create the nginx config and enable the site
   if [ -d /etc/nginx ] ; then
     cp nginx.in.conf nginx.conf
-    sedeasy /var/www/caleydo_app ${wd} nginx.conf
+    sedeasy /var/www/caleydo_app ${basedir} nginx.conf
     sedeasy caleydo_app ${name} nginx.conf
-    sudo ln -s ${wd}/nginx.conf /etc/nginx/sites-available/${name}
-    sudo ln -s ${wd}/nginx.conf /etc/nginx/sites-enabled/${name}
+    sudo ln -s ${basedir}/nginx.conf /etc/nginx/sites-available/${name}
+    sudo ln -s ${basedir}/nginx.conf /etc/nginx/sites-enabled/${name}
     sudo service nginx restart
+  fi
+}
+
+function remove_server_config {
+  local name=$1
+  sudo rm /etc/supervisor/conf.d/${name}.conf
+  #reread the list of elements
+  sudo supervisorctl reread
+  sudo supervisorctl update
+
+  #remove the nginx config and disable the site
+  if [ -d /etc/nginx ] ; then
+    sudo service nginx stop
+    sudo rm /etc/nginx/sites-available/${name}
+    sudo rm /etc/nginx/sites-enabled/${name}
+    sudo service nginx start
   fi
 }
 
@@ -69,7 +90,6 @@ function activate_virtualenv {
 
 function create_virtualenv {
   echo "--- creating virtual environment ---"
-  wd="`pwd`"
   if hash virtualenv 2>/dev/null; then
      echo "virtualenv already installed"
   else
@@ -83,6 +103,11 @@ function deactivate_virtualenv {
   deactivate
 }
 
+function remove_virtualenv {
+  echo "--- removing virtual environment ---"
+  rm -rf venv
+}
+
 function create_run_script {
     echo "#!/usr/bin/env bash
 
@@ -90,20 +115,13 @@ function create_run_script {
 " > run.sh
 }
 
+function manage_server {
+  local cmd=${1:-stop}
+  sudo supervisorctl caleydo_web ${stop}
+}
 
-#command switch
-case "$1" in
-update)
-  install_apt_dependencies
-  activate_virtualenv
-  install_pip_dependencies
-  deactivate_virtualenv
-  ;;
-create_config)
-  name=${PWD##*/}
-  create_server_config ${name}
-  ;;
-*)
+function setup {
+  echo "setup"
   install_apt_dependencies
   create_virtualenv
   install_pip_dependencies
@@ -113,5 +131,48 @@ create_config)
   create_server_config ${name}
 
   deactivate_virtualenv
+}
+
+function update {
+  echo "update"
+
+  manage_server stop
+
+  install_apt_dependencies
+  activate_virtualenv
+  install_pip_dependencies
+  deactivate_virtualenv
+
+  manage_server start
+}
+
+function uninstall {
+  echo "uninstall"
+  manage_server stop
+
+  name=${PWD##*/}
+  remove_server_config ${name}
+
+  remove_virtualenv
+}
+
+function create_config {
+  name=${PWD##*/}
+  create_server_config ${name}
+}
+
+#command switch
+case "$1" in
+update)
+  update
+  ;;
+create_config)
+  create_config
+  ;;
+uninstall)
+  uninstall
+  ;;
+*)
+  setup
   ;;
 esac
