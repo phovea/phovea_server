b3be3c7df2acd51c02119c3a40603626181ba01c
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-09-15 14:18:41 +0200

    dont deliver via static interface: py, pyc, config.json, _deploy/*, package.json

--- mainapp.py
+++ mainapp.py
@@ -69,11 +69,21 @@ def bowercomponents(path):
 def deliver_plugins(app, path):
   return flask.redirect('/' + path)
 
+import re
+black_list = re.compile(r'(.*\.(py|pyc|gitignore|gitattributes)|(\w+)/((config|package)\.json|_deploy/.*))')
+
+def is_on_black_list(path):
+  #print 'check',path,black_list.match(path) is not None
+  return black_list.match(path) is not None
+
 @app.route('/<path:path>')
 def deliver(path):
   print path
   if path.endswith('/'):
     path += 'index.html'
+  if is_on_black_list(path):
+    return 'This page does not exist', 404
+
   for d in caleydo_server.config.getlist('pluginDirs','caleydo_server'):
     d = os.path.abspath(d)
     dpath = flask.safe_join(d, path)
