e98052d6ed76471599dbe01c5beb73ca0e942832
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-09-22 14:47:26 +0200

    Caleydo/caleydo_web_container#109 work on server changes

--- _plugin_parser.py
+++ _plugin_parser.py
@@ -1,8 +1,6 @@
-import re
 import json
 import os
 import os.path
-from collections import OrderedDict
 from _utils import *
 
 import caleydo_server.config
@@ -25,11 +23,6 @@ def is_disabled_extension(extension, extension_type, p):
       return re.match(disable, extension['id'])
     return all(check_elem(k,v) for k,v in disable.iteritems())
   return any(map(check, cc.disable['extensions']))
-
-def _resolve_client_config(config, vars):
-  config = replace_variables(config, vars)
-  config = unpack_eval(config)
-  return config
 
 def _resolve_server_config(d, vars = {}):
   import six
@@ -55,193 +48,27 @@ def _extend(target, w):
       target[k] = v
   return target
 
-def _list_dirs(dd):
-  if not os.path.isdir(dd):
-      return []
-  return [d for d in os.listdir(dd) if not os.path.isfile(os.path.join(dd,d))]
-
-def _resolve_conflicts(dependencies):
-  r = dict()
-  for k,v in dependencies.iteritems():
-    if isinstance(v, list):
-      print 'resolving versions of ',k,v,'->', ' '.join(v)
-      r[k] = ' '.join(v)
-    else:
-      r[k] = v #single hit
-  return r
-
 class Plugin(object):
-  def __init__(self, plugindir, p, desc):
-    self.id = p
-    self.name = desc.get('name', p)
-    self.version = desc.get('version', '0.0.1')
-    self.folder = os.path.join(plugindir, p)
-    self.folder_name = p
+  def __init__(self, desc):
+    self.id = desc['id']
+    self.name = desc['name']
+    self.version = desc['version']
+    self.folder = desc['folder']
+    self.folder_name = os.path.basename(self.folder)
 
 class PluginMetaData(object):
   def __init__(self):
+    with file('./registry.json') as f:
+      data = json.load(f)
 
-    self.plugins = []
-    self.caleydo_client_plugins = []
-    self.caleydo_server_plugins = []
-
-    self.baseDir = cc.dir
-    self.ignored_bower_dependencies = []
-
-    self.requirejs_config = {
-      'paths': OrderedDict(),
-      'map': OrderedDict(),
-      'config': OrderedDict()
-    }
-    self._bower_configs = None
-
-  def _add_client_extension(self, extensions, plugin):
-    if not isinstance(extensions, list):
-      extensions = [ extensions ]
-    def fill(p):
-      p['folder'] = plugin.folder_name
-      if 'id' not in p:
-        p['id'] = plugin.id
-      if 'version' not in p:
-        p['version'] = plugin.version
-      if 'name' not in p:
-        p['name'] = plugin.name
-      return p
-    for p in extensions:
-      extension = fill(p)
-      if not is_disabled_extension(extension, 'web', plugin):
-        self.caleydo_client_plugins.append(extension)
-
-  def _add_server_extension(self, extensions, plugin):
-    if not isinstance(extensions, list):
-      extensions = [ extensions ]
-    def fill(p):
-      p['folder'] = plugin.folder_name
-      if 'id' not in p:
-        p['id'] = plugin.id
-      if 'version' not in p:
-        p['version'] = plugin.version
-      if 'name' not in p:
-        p['name'] = plugin.name
-      return p
-    for p in extensions:
-      extension = fill(p)
-      if not is_disabled_extension(extension, 'python', plugin):
-        self.caleydo_server_plugins.append(extension)
-
-  def _add_requirejs_config(self, rconfig, d):
-    _extend(self.requirejs_config, rconfig)
-
-  def _config_requirejs_bower(self, rconfig, d):
-    if 'ignore' in rconfig:
-      self.ignored_bower_dependencies.extend(rconfig['ignore'])
-
-  def add_plugin(self, plugindir, d):
-    metadata_file_abs = plugindir + d + cc.metadata_file
-    if not os.path.exists(metadata_file_abs):
-      return
-    print 'add plugin ' + metadata_file_abs
-    with open(metadata_file_abs, 'r') as f:
-      metadata = json.load(f)
-      p = Plugin(plugindir, d, metadata)
-
-      if is_disabled_plugin(p):
-        return
-
-      self.plugins.append(p)
-      if 'caleydo' in metadata:
-        c = metadata['caleydo']
-        if 'plugins' in c:
-          pp = c['plugins']
-          if 'web' in pp:
-            self._add_client_extension(pp['web'], p)
-          if 'python' in pp:
-            self._add_server_extension(pp['python'], p)
-        if 'requirejs-config' in c:
-          self._add_requirejs_config(c['requirejs-config'], d)
-        if 'requirejs-bower' in c:
-          self._config_requirejs_bower(c['requirejs-bower'], d)
-
-
-  def _add_bower_requirejs_config(self, d, scripts, cssfiles):
-    metadata_file_abs = self.baseDir + cc.bower_components + '/' + d + '/.bower.json'
-    print 'add bower dependency ' + metadata_file_abs
-    with open(metadata_file_abs, 'r') as f:
-      metadata = json.load(f)
-    if 'main' in metadata:
-      script = metadata['main']
-      if isinstance(script, list):
-        script = script[0]
-      if re.match(r'.*\.js$', script):
-        scripts[d] = d + '/' + script[:len(script) - 3]
-      elif re.match(r'.*\.css$', script):
-        cssfiles[d] = d + '/' + script[:len(script) - 4]
-
-  def derive_bower_requirejs_config(self):
-    print 'derive bower config'
-    scripts = dict()
-    cssfiles = dict()
-    for d in _list_dirs(self.baseDir + cc.bower_components):
-      if d in self.ignored_bower_dependencies:
-        continue
-      self._add_bower_requirejs_config(d, scripts, cssfiles)
-
-    self._bower_configs = scripts, cssfiles
-
-  def to_requirejs_config_file(self, main_file=None, base_url=None):
-    ccw = caleydo_server.config.view('caleydo_core')
-
-    if main_file is None:
-      main_file = ccw.mainFile
-    if base_url is None:
-      base_url = ccw.baseUrl
-
-    c = {
-      'baseUrl': base_url,
-      'paths': OrderedDict(),
-      'map': OrderedDict(),
-      'deps': [ main_file ],
-      'config': OrderedDict()
-    }
-    #extend with the stored one
-    _extend(c, self.requirejs_config)
-
-    #inject caleydo registry information
-    c['config'][ccw.configPrefix + 'caleydo_core/main'] = {
-      'apiUrl': ccw.apiPrefix,
-      'apiJSONSuffix': ccw.apiSuffix,
-      'registry': dict(baseUrl=base_url, extensions=self.caleydo_client_plugins)
-    }
-
-    #inject bower dependencies
-    for d,script in self._bower_configs[0].iteritems():
-      value = base_url+ccw.bower_components_url + '/' + script
-      c['paths'][d] = value
-    for d,css in self._bower_configs[1].iteritems():
-      value = self.requirejs_config['map']['*']['css'] + '!' + base_url+ccw.bower_components_url + '/' + css
-      c['map']['*'][d] = value
-
-    c = json.dumps(c, indent=2)
-    variables = {
-      'baseUrl': base_url+ccw.bower_components_url
-    }
-    c = _resolve_client_config(c, variables)
-
-    full = '/*global require */\r\nrequire.config(' + c + ');'
-    return full
+    self.plugins = filter(lambda x: not is_disabled_plugin(x), map(Plugin, data['plugins']))
+    by_folder= { p.folder : p for p in self.plugins }
+    self.caleydo_server_plugins = filter(lambda x : not is_disabled_extension(x, 'python', by_folder[x.folder]), data['extensions']['python'])
 
   @property
   def server_extensions(self):
     return _resolve_server_config(self.caleydo_server_plugins)
 
-  def parse_dirs(self, plugindirs):
-    for d in plugindirs:
-      for f in _list_dirs(d):
-          self.add_plugin(d, f)
-
 def parse():
   p = PluginMetaData()
-  p.parse_dirs(cc.getlist('pluginDirs'))
-  p.derive_bower_requirejs_config()
-
   return p
--- mainapp.py
+++ mainapp.py
@@ -6,59 +6,6 @@ import caleydo_server.config
 
 app = flask.Flask(__name__)
 
-@app.route('/')
-def index(standard_app = caleydo_server.config.get('default_app','default_app',None)):
-  if standard_app is not None:
-    return flask.redirect(standard_app+'/')
-
-  apps = [o.id for o in caleydo_server.plugin.plugins() if os.path.exists(os.path.join(o.folder, 'index.html'))]
-  if len(apps) == 1:
-    return flask.redirect(apps[0] + '/')
-  #generate a list of all known one
-  text = ['<!DOCTYPE html><html><head lang="en"><meta charset="UTF-8"><title>Caleydo Web Apps</title></head><body><h1>Caleydo Web Apps</h1><ul>']
-  text.extend(['<li><a href="' + a + '/">' + a + '</a></li>' for a in apps])
-  text.append('</li></body></html>')
-  return '\n'.join(text)
-
-@app.route('/apps.html')
-def apps():
-  return index()
-
-#deliver config
-@app.route('/config-gen.js')
-def genconfig():
-  #for which app main file
-  application = flask.request.args.get('app', None)
-  context = flask.request.args.get('context',None)
-  reg = generate_config(application, context)
-  #to specify the mime type even if it is a text
-  return flask.Response(response=reg, status=200, mimetype='application/javascript')
-
-def generate_config(application = None, context = None):
-  reg = caleydo_server.plugin.metadata().to_requirejs_config_file(application, context)
-  return reg
-
-def generate_wrapper(application=None, context = None):
-  #<script data-main="/config-gen.js" src="/bower_components/requirejs/require.js"></script>
-  base = "(function() {{ \
-    var headID = document.getElementsByTagName('head')[0]; \
-    var newScript = document.createElement('script'); \
-    newScript.type = 'text/javascript'; \
-    newScript.src = '{1}/bower_components/requirejs/require.js'; \
-    newScript.setAttribute('data-main','{1}/config-gen.js{0}'); \
-    headID.appendChild(newScript); \
-  }}())"
-  code = base.format('' if application is None else '?app='+application, '' if context is None else context)
-  return code
-
-@app.route('/caleydo_web.js')
-def gencore():
-  #for which app main file
-  application = flask.request.args.get('app', None)
-  context = flask.request.args.get('context',None)
-  code = generate_wrapper(application, context)
-  return flask.Response(response=code, mimetype='application/javascript')
-
 #deliver bower
 @app.route('/bower_components/<path:path>')
 def bowercomponents(path):
@@ -92,17 +39,6 @@ def deliver(path):
       print 'sending',dpath
       return flask.send_from_directory(d, path)
   return 'This page does not exist', 404
-
-def dump_generated_files(target_dir, application, context):
-  with open(os.path.join(target_dir,'config-gen.js'),'w') as f :
-    f.write(generate_config(application, context))
-  with open(os.path.join(target_dir,'caleydo_web.js'),'w') as f :
-    f.write(generate_wrapper(application, context))
-  index_content = index(application)
-  with open(os.path.join(target_dir,'index.html'),'w') as f :
-    f.write(index_content)
-  with open(os.path.join(target_dir,'apps.html'),'w') as f :
-    f.write(index_content)
 
 def default_app():
   return app
