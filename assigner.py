f19ab78835196200cf16f2a62eb2edb30d9af4a7
Samuel Gratzl <samuel_gratzl@gmx.at>
2015-12-15 14:41:19 +0100

    add db assigner (store assignments in a cache)

--- assigner.py
+++ assigner.py
@@ -33,7 +33,44 @@ class MemoryIDAssigner(object):
       return i
     return [ add(id) for id in ids]
 
-_assigner = MemoryIDAssigner()
+
+class DBIDAssigner(object):
+  def __init__(self):
+    import anydbm
+    import caleydo_server.config
+    self._db = anydbm.open(caleydo_server.config.get('caleydo_server.dataDir')+'/mapping.dbm','c')
+
+  @staticmethod
+  def to_forward_key(idtype, identifier):
+    return idtype + '2id.' + str(identifier)
+
+  @staticmethod
+  def to_backward_key(idtype, id):
+    return 'id2' + idtype + '.' + str(id)
+
+  def unmap(self, uids, idtype):
+    def lookup(id):
+      key = self.to_backward_key(idtype, id)
+      return self._db.get(key, None)
+    return map(lookup, uids)
+
+  def __call__(self, ids, idtype):
+    """
+     return the integer index ids for the given ids in the given idtype
+    """
+
+    def add(id) :
+      key = self.to_forward_key(idtype, id)
+      i = int(self._db.get(key, '-1'))
+      if i < 0: #not yet part of
+        i = int(self._db.get(idtype,'-1'))+1
+        self._db[key] = str(i)
+        self._db[self.to_backward_key(idtype, i)] = str(id)
+        self._db[idtype] = str(i)
+      return i
+    return map(add, ids)
+
+_assigner = DBIDAssigner()
 
 def create():
   return _assigner
