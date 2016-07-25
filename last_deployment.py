8afcf64a7559409be55c53baf2e092f772f0b08c
Holger Stitz <holger.stitz@jku.at>
2016-07-25 10:51:32 +0200

    Show last deployment date in the about dialog. Caleydo/caleydo_web_container#150

---
+++ last_deployment.py
@@ -0,0 +1,23 @@
+import flask
+import os.path
+import datetime
+import caleydo_server.util
+
+last_deployment = flask.Flask(__name__)
+
+def modification_date(filename):
+    t = os.path.getmtime(filename)
+    return datetime.datetime.fromtimestamp(t)
+
+"""
+ Reads the modification date of the caleydo_web_container/package.json
+"""
+@last_deployment.route('/', methods=['GET'])
+def _last_deployment():
+  path =  os.path.normpath(os.path.join(os.getcwd(), 'package.json'))
+  date = modification_date(path)
+  print 'modification date of ' + path + ' = ' + str(date)
+  return caleydo_server.util.jsonify(dict(timestamp = date))
+
+def create_last_deployment():
+  return last_deployment
