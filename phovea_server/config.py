c127dbc2bf7f5bdbd121c15e46d3e862d505cf68
Samuel Gratzl <samuel_gratzl@gmx.at>
2017-07-13 11:03:16 +0200

    fix reapplying changed config before creating final one

--- phovea_server/config.py
+++ phovea_server/config.py
@@ -16,6 +16,7 @@ import logging
 
 _log = logging.getLogger(__name__)
 _c = {}
+_preMergeChanges = {}
 
 
 def get(item, section=None, default=None):
@@ -39,12 +40,24 @@ def get(item, section=None, default=None):
 
 
 def set(item, value, section=None):
+  global _c, _preMergeChanges
   key = item if section is None else section + '.' + item
   keys = key.split('.')
   act = _c
   for p in keys[0:-1]:
-    act = act.get(p, {})
+    if p not in act:
+      act[p] = {}
+    act = act[p]
   act[keys[len(keys) - 1]] = value
+
+  if _preMergeChanges is not None:
+    # keep track of changes that were done before the recreation of the configuration
+    act = _preMergeChanges
+    for p in keys[0:-1]:
+      if p not in act:
+        act[p] = {}
+      act = act[p]
+    act[keys[len(keys) - 1]] = value
 
 
 def getint(item, section=None, default=0):
@@ -128,7 +141,7 @@ _init_config()
 
 def merge_plugin_configs(plugins):
   # merge all the plugins
-  global _c
+  global _c, _preMergeChanges
   _c = {}
 
   for plugin in plugins:
@@ -142,3 +155,7 @@ def merge_plugin_configs(plugins):
   if os.path.exists(global_):
     with codecs.open(global_, 'r', 'utf-8') as fi:
       extend(_c, jsoncfg.loads(fi.read()))
+
+  # merge changes done before the merge
+  extend(_c, _preMergeChanges)
+  _preMergeChanges = None
