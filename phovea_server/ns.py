bb821acd52699ee70bd301057f73146dba8c5cbb
Samuel Gratzl <samuel_gratzl@gmx.at>
2018-06-23 16:54:26 +0200

    simplify etag generation

--- phovea_server/ns.py
+++ phovea_server/ns.py
@@ -39,7 +39,6 @@ def no_cache(f):
 def etag(f):
   """Add entity tag (etag) handling to the decorated route."""
   import functools
-  import hashlib
 
   @functools.wraps(f)
   def wrapped(*args, **kwargs):
@@ -58,39 +57,6 @@ def etag(f):
     if rv.status_code != 200 or rv.direct_passthrough or not rv.implicit_sequence_conversion:
       return rv
 
-    # compute the etag for this request as the MD5 hash of the response
-    # text and set it in the response header
-    etag = '"' + hashlib.sha1(rv.get_data()).hexdigest() + '"'
-    rv.headers['ETag'] = etag
-    rv.headers['Cache-Control'] = 'must-revalidate'
-
-    # handle If-Match and If-None-Match request headers if present
-    if_match = request.headers.get('If-Match')
-    if_none_match = request.headers.get('If-None-Match')
-    # weak version with prefixed W/
-    weak_etag = 'W/' + etag
-
-    if if_match:
-      # only return the response if the etag for this request matches
-      # any of the etags given in the If-Match header. If there is no
-      # match, then return a 412 Precondition Failed status code
-      etag_list = [tag.strip() for tag in if_match.split(',')]
-      if etag not in etag_list and weak_etag not in etag_list and '*' not in etag_list:
-        response = jsonify({'status': 412, 'error': 'precondition failed',
-                            'message': 'precondition failed'})
-        response.headers['Cache-Control'] = 'must-revalidate'
-        response.status_code = 412
-        return response
-    elif if_none_match:
-      # only return the response if the etag for this request does not
-      # match any of the etags given in the If-None-Match header. If
-      # one matches, then return a 304 Not Modified status code
-      etag_list = [tag.strip() for tag in if_none_match.split(',')]
-      if etag in etag_list or weak_etag in etag_list or '*' in etag_list:
-        response = jsonify({'status': 304, 'error': 'not modified',
-                            'message': 'resource not modified'})
-        response.headers['Cache-Control'] = 'must-revalidate'
-        response.status_code = 304
-        return response
-    return rv
+    rv.add_etag()
+    return rv.make_conditional(request)
   return wrapped
