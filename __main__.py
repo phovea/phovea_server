e29dfdfeffefd0bac1ab6be73b7a68ad56b11810
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2019-08-22 08:01:04 +0200

    Fix for relative imports
    added /phovea/phovea_server/phovea_server to sys.path

--- __main__.py
+++ __main__.py
@@ -4,10 +4,14 @@
 # Licensed under the new BSD license, available at http://caleydo.org/license
 ###############################################################################
 
-from phovea_server import launch
+import os
+import sys
+sys.path.insert(0, os.path.abspath('/phovea/phovea_server/phovea_server'))
 
 if __name__ == '__main__':
+  import launch
   launch.run()
 else:
+  import launch
   application = launch.create_embedded()
   print('run as embedded version: %s', application)
