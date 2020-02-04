4f8f3b039ad7428f779295722b34c7220c31e429
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-02-04 08:53:12 +0100

    refactor SetEncoder

--- phovea_server/set_encoder.py
+++ phovea_server/set_encoder.py
@@ -5,14 +5,10 @@ This encoder is required to handle changes of data types in Python 3.7 by adding
 
 class SetEncoder(object):
   def __contains__(self, obj):
-    if isinstance(obj, set):
-      return True
-    return False
+    return isinstance(obj, set)
 
   def __call__(self, obj, base_encoder):
-    if isinstance(obj, set):
-      return list(obj)
-    return None
+    return list(obj)
 
 
 encoder = SetEncoder()
