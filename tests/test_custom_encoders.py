1261c0c9b07351a14f18a5394d178b7d381d0f77
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-02-14 12:02:52 +0100

    add simple tests for custom encoders

---
+++ tests/test_custom_encoders.py
@@ -0,0 +1,9 @@
+from phovea_server.util import jsonify
+
+
+class TestCustomEncoders:
+  def test_sets(self):
+    assert jsonify(set()) == []
+
+  def test_nan_values(self):
+    assert float('nan') == None
