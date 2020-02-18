67ded4710d5cd4b43b2289f338366b95d5426aee
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-02-18 09:47:21 +0100

    use self

--- tests/test_custom_encoders.py
+++ tests/test_custom_encoders.py
@@ -5,6 +5,6 @@ class TestCustomEncoders:
   # def test_sets(self):
   #  assert jsonify(set()) == []
 
-  def test_nan_values():
+  def test_nan_values(self):
     test_var = float('nan')
     assert jsonify([test_var, 'This', 'is', 'a', 'very', 'complex', 'test', 'case', test_var]) == [None, "This", "is", "a", "very", "complex", "test", "case", None]
