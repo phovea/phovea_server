42ea74593166c9272cb3cb3d4e8497d93c26da0e
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-02-18 09:44:06 +0100

    fix nan test case

--- tests/test_custom_encoders.py
+++ tests/test_custom_encoders.py
@@ -2,8 +2,9 @@ from phovea_server.util import jsonify
 
 
 class TestCustomEncoders:
-  def test_sets(self):
-    assert jsonify(list(set())) == []
+  # def test_sets(self):
+  #  assert jsonify(set()) == []
 
-  def test_nan_values(self):
-    assert float('nan') is None
+  def test_nan_values():
+    test_var = float('nan')
+    assert jsonify([test_var, 'This', 'is', 'a', 'very', 'complex', 'test', 'case', test_var]) == [null, "This", "is", "a", "very", "complex", "test", "case", null]
