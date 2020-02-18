ff2cca9ea85e833ead2a36907a8c7399a3b4a737
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-02-18 08:33:43 +0100

    add list() to set() to make it json serializable

--- tests/test_custom_encoders.py
+++ tests/test_custom_encoders.py
@@ -3,7 +3,7 @@ from phovea_server.util import jsonify
 
 class TestCustomEncoders:
   def test_sets(self):
-    assert jsonify(set()) == []
+    assert jsonify(list(set())) == []
 
   def test_nan_values(self):
     assert float('nan') is None
