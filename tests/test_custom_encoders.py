18ddee546e2182a61744826bf0d6621096c7dd28
dvvanessastoiber <vanessa.stoiber@datavisyn.io>
2020-02-27 08:51:45 +0100

    test NaNEncoder only

--- tests/test_custom_encoders.py
+++ tests/test_custom_encoders.py
@@ -1,13 +1,7 @@
-from phovea_server.util import JSONExtensibleEncoder, to_json
-import json
+from phovea_server.util import to_json
 
 
 class TestCustomEncoders:
-  def test_sets(self):
-    test_set = set()
-    test_resultset = json.dumps(test_set, cls=JSONExtensibleEncoder)
-    assert test_resultset == '[]'
-
   def test_nan_values(self):
     test_var = float('nan')
     test_result = to_json(dict(myNum=test_var))
